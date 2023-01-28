56f22bb69fe89e0a392399d1ccc9d7a9e11effe4
Fasil11 <seifufasil@gmail.com>
2023-01-28 12:33:16 -0800

    tasks

--- errors1.c
+++ errors1.c
@@ -1,5 +1,7 @@
 #include "shell.h"
 
+
+
 /**
  * _erratoi - converts a string to an integer
  * @s: the string to be converted
@@ -14,22 +16,38 @@ int _erratoi(char *s)
 
 	unsigned long int result = 0;
 
+
 	if (*s == '+')
+
 		s++;  /* TODO: why does this make main return 255? */
+
 	for (i = 0;  s[i] != '\0'; i++)
+
 	{
+
 		if (s[i] >= '0' && s[i] <= '9')
+
 		{
+
 			result *= 10;
+
 			result += (s[i] - '0');
+
 			if (result > INT_MAX)
+
 				return (-1);
+
 		}
+
 		else
+
 			return (-1);
+
 	}
+
 	return (result);
 }
+
 
 /**
  * print_error - prints an error message
@@ -42,15 +60,22 @@ int _erratoi(char *s)
 void print_error(info_t *info, char *estr)
 
 {
-
 	_eputs(info->fname);
+
 	_eputs(": ");
+
 	print_d(info->line_count, STDERR_FILENO);
+
 	_eputs(": ");
+
 	_eputs(info->argv[0]);
+
 	_eputs(": ");
+
 	_eputs(estr);
+
 }
+
 
 /**
  * print_d - function prints a decimal (integer) number (base 10)
@@ -62,34 +87,61 @@ void print_error(info_t *info, char *estr)
 int print_d(int input, int fd)
 
 {
+
 	int (*__putchar)(char) = _putchar;
 
 	int i, count = 0;
 
 	unsigned int _abs_, current;
 
+
+
 	if (fd == STDERR_FILENO)
+
 		__putchar = _eputchar;
+
 	if (input < 0)
+
 	{
+
 		_abs_ = -input;
+
 		__putchar('-');
+
 		count++;
 	}
+
 	else
+
 		_abs_ = input;
+
 	current = _abs_;
+
 	for (i = 1000000000; i > 1; i /= 10)
+
+	{
+
 		if (_abs_ / i)
+
 		{
+
 			__putchar('0' + current / i);
+
 			count++;
+
 		}
+
 		current %= i;
+
 	}
+
 	__putchar('0' + current);
+
 	count++;
+
+
 	return (count);
+
 }
 
 /**
@@ -97,13 +149,13 @@ int print_d(int input, int fd)
  * @num: number
  * @base: base
  * @flags: argument flags
+ *
  * Return: string
  */
 
 char *convert_number(long int num, int base, int flags)
 
 {
-
 	static char *array;
 
 	static char buffer[50];
@@ -123,9 +175,6 @@ char *convert_number(long int num, int base, int flags)
 		n = -num;
 
 		sign = '-';
-
-
-
 	}
 
 	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
@@ -134,6 +183,8 @@ char *convert_number(long int num, int base, int flags)
 
 	*ptr = '\0';
 
+
+
 	do	{
 
 		*--ptr = array[n % base];
@@ -141,28 +192,39 @@ char *convert_number(long int num, int base, int flags)
 		n /= base;
 
 	} while (n != 0);
-	if (sign)
-		*--ptr = sign;
-	return (ptr);
 
+
+	if (sign)
+
+		*--ptr = sign;
+
+	return (ptr);
 }
+
+
 
 /**
  * remove_comments - function replaces first instance of '#' with '\0'
  * @buf: address of the string to modify
+ *
  * Return: Always 0;
  */
 
 void remove_comments(char *buf)
 
 {
+
 	int i;
 
+
 	for (i = 0; buf[i] != '\0'; i++)
+
 		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+
 		{
 			buf[i] = '\0';
-			break;
-		}
 
+			break;
+
+		}
 }
--- shell.h
+++ shell.h
@@ -155,15 +155,14 @@ typedef struct passinfo
 } info_t;
 
 #define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+		0, 0, 0}
 
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, \
-	0, 0, 0}
-
-	/**
-	 * struct builtin - contains a builtin string and related function
-	 * @type: the builtin command flag
-	 * @func: the function
-	 */
+/**
+ * struct builtin - contains a builtin string and related function
+ * @type: the builtin command flag
+ * @func: the function
+ */
 	typedef struct builtin
 {
 	char *type;
